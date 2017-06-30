403bc3d74a92afe74d05acccc6d7e4f44b7d4762
JustDontDie <justdontdiebusiness@gmail.com>
2017-06-30 18:51:11 -0500

    Reworked commands and bot status

--- bot.js
+++ bot.js
@@ -1,25 +1,42 @@
 // Requires
 const Discord = require('discord.js');
 
+const PREFIX = "!";
+
 // Create an instance of a Discord client
 var client = new Discord.Client();
 
-// The bot is ready
 client.on('ready', () => {
   console.log('Bot Started');
-})
+  
+  var embed = new Discord.RichEmbed();
+  embed.setColor("#40C03D");
+  embed.addField("Bot Status", "The bot is now online!");
+  embed.setFooter("Your bot is now ready to do what it was made to do.");
 
-client.on('ready', () => {
-  client.user.setGame('Moderating');
+  client.channels.get("330410520622530562").sendEmbed(embed);
+});
+
+client.on('disconnect', () => {
+  var embed = new Discord.RichEmbed();
+  embed.setColor("#F22213");
+  embed.addField("Bot Status", "The bot has gone offline!");
+  embed.setFooter("The bot may have ran into an error. If you know it didn't, you're good to go. Else, check Heroku for more information");
+
+  client.channels.get("330410520622530562").sendEmbed(embed);
 });
 
 client.on('message', function(message) {
-  if (message.channel.name === "trial_help") {
-    if (message.content.startsWith("!help ")) {
-      message.channel.send(`${message.author.username} is requesting assistance! @everyone`, () => {
-        console.log("Sent a message in the " + message.channel.name + " text channel");
-      });
-    }
+  if (!message.content.startsWith(PREFIX)) return;
+
+  var command = message.content.replace("!", "").split(" ");
+
+  switch (command[0].toLowerCase()) {
+    case "help":
+      client.channels.get("329985394949226506").send(`@everyone, ${message.author.username} is requesting assistance`);
+      break;
+    default:
+      break;
   }
 });
 
